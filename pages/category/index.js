ba42069a1d0c14c35f6e3ac2d00881f979a92c76
thomas.lin <thomaslwq@gmail.com>
2020-02-08 15:08:55 +0800

    分类页面&&商品列表

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,11 +1,15 @@
 // pages/category/index.js
+import { request } from "../../request/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    categories:[],//分类数据
+    rightContent:[],
+    selectIndex:0, //默认选中第一个分类
+    scroll_top:0 //滚动条的位置
   },
 
   /**
@@ -21,12 +25,65 @@ Page({
   onReady: function () {
 
   },
+  handleItemClick:function(e){
+    const index = e.currentTarget.dataset.index;
+    const scroll_top = 0;
+    this.setData({
+      rightContent:this.data.categories[index].children,
+      selectIndex:index,
+      scroll_top: scroll_top
+    })
+  },
+  /* 
+  1. 每次拿到数据之后，先把数据存在存在缓存里面 storage
+  同时设置一下缓存时间
+  2. 当下次进来的时候
+    a.如果在有效时间内容，我们就不发请求；
+    b.如果过了时间，那么就再次请求数据
+   */
+  getCategories: async function(){
+    const categories = await  request({
+      url:"/categories"
+    });
+    const rightContent = categories[0].children;
+    const selectIndex = 0;
+    wx.setStorageSync("carts", {
+      time:Date.now(),
+      data:categories
+    })
+    this.setData({
+      categories,rightContent,selectIndex
+    })
+    console.log(categories);
+  },
 
   /**
    * 生命周期函数--监听页面显示
    */
+  /* 
+      1. 先从缓存中获取数据
+      2. 判断数据是否过期，如果过期重新请求数据
+      3. 如果没有过期 直接使用数据
+   */
   onShow: function () {
+    let carts = wx.getStorageSync("carts");
+    if(!carts){
+      this.getCategories();
+    }else{
+      // 判断一下是否时间过期 10s 
+      if (Date.now() - carts.time > 1000 * 10){
+        this.getCategories();
+      }else{
+        //不过期 同时又请求过数据
+        let categories = carts.data;
+        const rightContent = categories[0].children;
+        const selectIndex = 0;
+        this.setData({
+          categories,rightContent,selectIndex
+        })
 
+      }
+    }
   },
 
   /**
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,139 @@
 // pages/goods_list/index.js
+import {
+  request
+} from "../../request/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: [{
+      id: 0,
+      value: "综合",
+      isActive: true
+    }, {
+      id: 1,
+      value: "销量",
+      isActive: false
+    }, {
+      id: 2,
+      value: "价格",
+      isActive: false
+    }],
+    goods:[]// 搜索的商品列表
+  },
+  QueryParams: {
+    pagenum: 1,
+    pagesize: 10
+  },
+  totalPages: 0,
+  // 处理tabs 传过来的参数
+  handleTabsChange: function(e) {
+    this.setData({
+      tabs: e.detail
+    })
+  },
+  //获取列表数据
+  getGoodsList: async function() {
+    const goods_list = await request({
+      url: "/goods/search",
+      data: this.QueryParams
+    })
+    // 总共有多少页
+    this.totalPages = Math.ceil(goods_list.total / this.QueryParams.pagesize);
+    this.setData({
+      goods: [...goods_list.goods,...this.data.goods]
+    })
+    //关闭下拉提示圈
+    wx.stopPullDownRefresh();
+    console.log(this.data.goods);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    //cid query
+    const pages = getCurrentPages();
+    const currentPage = pages[pages.length - 1];
+    console.log(currentPage.options);
+    const {
+      cid,
+      query
+    } = currentPage.options;
+    //如果页面有传 cid 参数过来
+    if (cid) {
+      this.QueryParams["cid"] = cid;
+    }
+    if (query) {
+      this.QueryParams["query"] = query;
+    }
+    this.getGoodsList();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
+  // 下拉 重新加载数据
+  onPullDownRefresh: function() {
+    //1. 清空原有的goods=[]
+    this.setData({
+      goods:[]
+    })
+    //2 把页面重置为1
+    this.QueryParams.pagenum = 1;
+    //重新发起请求
+    this.getGoodsList();
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
+  //触底 加载下一页
+  onReachBottom: function() {
+    if(this.totalPages >= this.QueryParams.pagenum){
+      this.QueryParams.pagenum ++;
+      this.getGoodsList();
+    }else{
+      wx.showToast({
+        title: '已经没有下一页的数据了',
+      })
+    }
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
