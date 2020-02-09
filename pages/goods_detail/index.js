9cb924a5c6e7684020afffd99c24f6224a9afbb9
thomas.lin <thomaslwq@gmail.com>
2020-02-09 14:01:30 +0800

    添加商品详情-待完善

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,20 +1,40 @@
 // pages/goods_detail/index.js
+import { request } from "../../request/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods_detail:{} //用来存详情信息
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getGoodsInfo(options);
   },
-
+  //获取商品的详情信息
+  getGoodsInfo: async function(params){
+    // 1. 发送请求 获取产品详情信息
+    const goods_detail = await request({
+      url:"/goods/detail",
+      data:params
+    });
+    console.log(goods_detail);
+    this.setData({
+      goods_detail
+    })
+  },
+  //处理图片的点击预览事件
+  handleImageClick:function(e){
+    const { pics,current } = e.currentTarget.dataset;
+    wx.previewImage({
+      current:current,
+      urls: pics.map(v => v.pics_mid),
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -21,14 +21,14 @@ Page({
       value: "价格",
       isActive: false
     }],
-    goods:[]// 搜索的商品列表
+    goods: [] // 搜索的商品列表
   },
   QueryParams: {
     pagenum: 1,
     pagesize: 10
   },
   totalPages: 0,
-  // 处理tabs 传过来的参数
+  // 处理tabs 传过来的参数 tabs 存在 e.detail
   handleTabsChange: function(e) {
     this.setData({
       tabs: e.detail
@@ -43,7 +43,8 @@ Page({
     // 总共有多少页
     this.totalPages = Math.ceil(goods_list.total / this.QueryParams.pagesize);
     this.setData({
-      goods: [...goods_list.goods,...this.data.goods]
+      //追加
+      goods: [...this.data.goods,...goods_list.goods]
     })
     //关闭下拉提示圈
     wx.stopPullDownRefresh();
@@ -55,6 +56,14 @@ Page({
    */
   onLoad: function(options) {
     //cid query
+
+    /* 
+   PageObject[] getCurrentPages()
+  获取当前页面栈。数组中第一个元素为首页，最后一个元素为当前页面。
+   注意：
+ 不要尝试修改页面栈，会导致路由以及页面状态错误。
+ 不要在 App.onLaunch 的时候调用 getCurrentPages()，此时 page 还没有生成。
+     */
     const pages = getCurrentPages();
     const currentPage = pages[pages.length - 1];
     console.log(currentPage.options);
@@ -62,6 +71,10 @@ Page({
       cid,
       query
     } = currentPage.options;
+    //     const {
+    //   cid,
+    //   query
+    // } = options;
     //如果页面有传 cid 参数过来
     if (cid) {
       this.QueryParams["cid"] = cid;
@@ -107,7 +120,7 @@ Page({
   onPullDownRefresh: function() {
     //1. 清空原有的goods=[]
     this.setData({
-      goods:[]
+      goods: []
     })
     //2 把页面重置为1
     this.QueryParams.pagenum = 1;
@@ -120,10 +133,10 @@ Page({
    */
   //触底 加载下一页
   onReachBottom: function() {
-    if(this.totalPages >= this.QueryParams.pagenum){
-      this.QueryParams.pagenum ++;
+    if (this.totalPages >= this.QueryParams.pagenum) {
+      this.QueryParams.pagenum++;
       this.getGoodsList();
-    }else{
+    } else {
       wx.showToast({
         title: '已经没有下一页的数据了',
       })
